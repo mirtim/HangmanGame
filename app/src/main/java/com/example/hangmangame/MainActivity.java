c6ad24f7339d0292ad4019ba1875f3aa6b2b98cc
mirtim <mirtim@bu.edu>
2019-09-27 14:15:44 -0400

    created buttons and field for words, I will make the pictures for the game

--- app/src/main/java/com/example/hangmangame/MainActivity.java
+++ app/src/main/java/com/example/hangmangame/MainActivity.java
@@ -3,12 +3,109 @@ package com.example.hangmangame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private Button btnE;
+    private Button btnF;
+    private Button btnG;
+    private Button btnH;
+    private Button btnI;
+    private Button btnJ;
+    private Button btnK;
+    private Button btnL;
+    private Button btnM;
+    private Button btnN;
+    private Button btnO;
+    private Button btnP;
+    private Button btnQ;
+    private Button btnR;
+    private Button btnS;
+    private Button btnT;
+    private Button btnU;
+    private Button btnV;
+    private Button btnW;
+    private Button btnX;
+    private Button btnY;
+    private Button btnZ;
+    private TextView ltr1;
+    private TextView ltr2;
+    private TextView ltr3;
+    private TextView ltr4;
+    private TextView ltr5;
+    private Button btnGame;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnA = (Button) findViewById(R.id.btnA);
+        btnB = (Button) findViewById(R.id.btnB);
+        btnC = (Button) findViewById(R.id.btnC);
+        btnD = (Button) findViewById(R.id.btnD);
+        btnE = (Button) findViewById(R.id.btnE);
+        btnF = (Button) findViewById(R.id.btnF);
+        btnG = (Button) findViewById(R.id.btnG);
+        btnH = (Button) findViewById(R.id.btnH);
+        btnI = (Button) findViewById(R.id.btnI);
+        btnJ = (Button) findViewById(R.id.btnJ);
+        btnK = (Button) findViewById(R.id.btnK);
+        btnL = (Button) findViewById(R.id.btnL);
+        btnM = (Button) findViewById(R.id.btnM);
+        btnN = (Button) findViewById(R.id.btnN);
+        btnO = (Button) findViewById(R.id.btnO);
+        btnP = (Button) findViewById(R.id.btnP);
+        btnQ = (Button) findViewById(R.id.btnQ);
+        btnR = (Button) findViewById(R.id.btnR);
+        btnS = (Button) findViewById(R.id.btnS);
+        btnT = (Button) findViewById(R.id.btnT);
+        btnU = (Button) findViewById(R.id.btnU);
+        btnV = (Button) findViewById(R.id.btnV);
+        btnW = (Button) findViewById(R.id.btnW);
+        btnX = (Button) findViewById(R.id.btnX);
+        btnY = (Button) findViewById(R.id.btnY);
+        btnZ = (Button) findViewById(R.id.btnZ);
+        ltr1 = (TextView) findViewById(R.id.ltr1);
+        ltr2 = (TextView) findViewById(R.id.ltr2);
+        ltr3 = (TextView) findViewById(R.id.ltr3);
+        ltr4 = (TextView) findViewById(R.id.ltr4);
+        ltr5 = (TextView) findViewById(R.id.ltr5);
+        btnGame = (Button) findViewById(R.id.btnGame);
+
+        String[] words = {"apple", "phone", "chair"};              // create a list of words with 5 letters
+
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // pick a random word from words
+                // set each letter for each of 5 text views
+                // update the picture(I will make the pictures!)
+                // update the hind button
+                // enable all buttons
+
+            }
+        });
+
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
     }
 }
